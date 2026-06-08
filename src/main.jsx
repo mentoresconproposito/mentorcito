f806110539d1859d24340d0ac37d7c613cdb99ad
mentoresconproposito <mentoresconproposito@gmail.com>
2026-06-08 18:29:40 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import MentorAgent from './MentorAgent'
 import Dashboard from './Dashboard'
 import Privacidad from './Privacidad'
+import EmpresaAgent from './EmpresaAgent'
+import EmpresaDashboard from './EmpresaDashboard'
 
 function App() {
   var path = window.location.pathname;
-  if (path === '/dashboard') return <Dashboard />;
-  if (path === '/privacidad') return <Privacidad />;
+  if (path === '/dashboard')             return <Dashboard />;
+  if (path === '/privacidad')            return <Privacidad />;
+  if (path.startsWith('/empresa/'))      return <EmpresaDashboard />;
+  // Modo empresa en el agente: ?empresa=slug&token=xxx
+  var params = new URLSearchParams(window.location.search);
+  if (params.get('empresa') && params.get('token')) return <EmpresaAgent />;
   return <MentorAgent />;
 }
 
