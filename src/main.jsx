4fa1e87af678c54c42dc28540422e0c9b9fb91cd
mentoresconproposito <mentoresconproposito@gmail.com>
2026-06-08 14:05:15 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MentorAgent from './MentorAgent'
 import Dashboard from './Dashboard'
+import Privacidad from './Privacidad'
 
 function App() {
   var path = window.location.pathname;
   if (path === '/dashboard') return <Dashboard />;
+  if (path === '/privacidad') return <Privacidad />;
   return <MentorAgent />;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 )
